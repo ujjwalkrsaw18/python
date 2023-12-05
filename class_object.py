57f61571010a6254dc9af4d8721a227c5c0e5779
Ujjwal Kumar Saw <152911264+ujjwalkrsaw18@users.noreply.github.com>
2023-12-05 12:24:05 +0530

    Add files via upload

---
+++ class_object.py
@@ -0,0 +1,56 @@
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+class sum:
+    def abc(obj, a, b):
+        c=a+b
+        print("Sum of a and b:",c)
+
+obj=sum()
+obj.abc(a,b)
+
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+class sum:
+    def __init__(self,a,b):
+        c=a+b
+        print("Sum of a and b:",c)
+
+xx=sum(a,b)
+
+#wap to create a class of circle and calculate the area and perimeter of circle.
+r=int(input("Enter the radius of circle:"))
+class circle:
+    def __init__(self,r):
+        area=22/7*r*r
+        peri=2*22/7*r
+        print("Area of a circle is:",area)
+        print("Perimeter of a circle is:",peri)
+ra=circle(r)        
+#wap to create a person class include name country and dob determine a function for person age.
+
+class person:
+    def __init__(self)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
